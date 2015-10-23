05f92bac2f8f3bac9a58b10d62090102492b3378
nataliecodes <nataliepolen@gmail.com>
2015-10-23 17:15:39 -0400

    Update add it up and factorial solutions

--- week-4/factorial/my_solution.rb
+++ week-4/factorial/my_solution.rb
@@ -1,26 +1,24 @@
 # Factorial
 
-# I worked on this challenge [by myself, with: ].
-
+# I worked on this challenge with Nil.
 
 # Your Solution Below
 def factorial(number)
   if number == 0
-  return 1
+    return 1
   elsif (number < 0)
-  return false
+    return false
   elsif (number == 1)
-  return 1
+   return 1
   else
-  product = number
-  next_number = 0
-  while (number > 0)
-  next_number = number - 1
-  product = product * next_number
-  puts "number = #{number}, next_number = #{next_number}, and product = #{product}"
-  number = number - 1
-  break if number == 1
-  end
+    product = number
+    next_number = 0
+    while (number > 0)
+      next_number = number - 1
+      product = product * next_number
+      number = number - 1
+      break if number == 1
+    end
   return product
   end
 end
