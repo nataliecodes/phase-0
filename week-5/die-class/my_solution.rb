dc6b3b8c0f41b503f475391bc5670ac763c03413
nataliecodes <nataliepolen@gmail.com>
2015-11-02 11:47:05 -0500

    Update die class reflection and acct groups assignment

--- week-5/die-class/my_solution.rb
+++ week-5/die-class/my_solution.rb
@@ -71,22 +71,34 @@ end
 # What new Ruby methods did you implement? What challenges
 # and successes did you have in implementing them?
 
-The only new actual method I used was rand. My issue
-was that the Ruby docs weren't super obvious to me so
-I didn't realize I had to
+# The only new actual method I used was rand. My issue
+# was that the Ruby docs weren't super obvious to me so
+# I didn't realize I had to set roll to Random.new first.
+# After that, though, things went well.
 
 # What is a Ruby class?
 
-
+# A Ruby class is sort of like a general object definition.
+# The array class has certain info containers that behave
+# in a certain way, with a certain format, and certain
+# built-in methods.
 
 # Why would you use a Ruby class?
 
-
+# You use a Ruby class to better manipulate and calculate
+# stuff. Instead of just one long string of words with numbers
+# next to them, you can use a hash to order the list and
+# find ways to access certain pieces of information.
+# Code wouldn't be as readable without classes.
 
 # What is the difference between a local variable and an instance variable?
 
-
+# A local variable only exists between the "def" and "end"
+# of a method. An instance variable is accessible outside of
+# methods like this. Within a class, for example, you can
+# use instance variables among many methods and therefore
+# have the ability to work with this varialble many times.
 
 # Where can an instance variable be used?
 
-
+# It can be used anywhere within a designated class.
