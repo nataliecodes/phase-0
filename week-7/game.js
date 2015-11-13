94eafb3377ae8911de7cea74fa164f13504cfdd8
nataliecodes <nataliepolen@gmail.com>
2015-11-13 13:35:22 -0500

    Add empty game.js file

---
+++ week-7/game.js
@@ -0,0 +1,38 @@
+// Design Basic Game Solo Challenge
+
+// This is a solo challenge
+
+// Your mission description:
+// Overall mission: Guess the word or get 40 points
+// Goals: Guess letters without going over the allowed number of guesses (10), get points for each letter
+// Characters: Player
+// Objects: Player(points, guess), Answer(solved?)
+// Functions: Guess(letter), Check(letter/answer)
+
+// Pseudocode
+//
+//
+
+// Initial Code
+
+
+
+
+
+
+// Refactored Code
+
+
+
+
+
+
+// Reflection
+//
+//
+//
+//
+//
+//
+//
+//
