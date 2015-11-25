4fad8343286952e45003d4771fc9a16995719e96
nataliecodes <nataliepolen@gmail.com>
2015-11-25 15:50:02 -0500

    Add incomplete grocery list solution to file

--- week-9/review.js
+++ week-9/review.js
@@ -29,8 +29,6 @@ Iterate through each column to look for the letter and the number
 // Initial Solution
 
 
-var board
-
 // #DRIVER CODE (I.E. METHOD CALLS) GO BELOW THIS LINE
 // board = [[47, 44, 71, 8, 88],
 //         [22, 69, 75, 65, 73],
@@ -39,3 +37,50 @@ var board
 //         [75, 70, 54, 80, 83]]
 
 // new_game = BingoBoard.new(board)
+
+
+// ------------------------------------------------
+
+/* 9.2.1 JavaScript Grocery List
+
+Pseudocode
+
+1. Create a new object that will contain pairs of information: the grocery item and the quantity
+2. Be able to add items and quantities to the list
+  a. Create a function to do this. Have the function take an item and quantity pair that can be forked into the existing list
+3. Create a function to remove an item from the list
+  a. The function will take an item's name as an argument
+  b. Then, the function will iterate through each item on the list
+  c. Once it reaches that list item, it will delete that item at the proper index
+  d. Print an error message if someone tries to pass in an item that isn't already on the list
+4. Create a function to update quantities already in the list
+5. Print the list to look nice
+
+*/
+
+var groceryList = [["apples", 2],
+                   ["bananas", 3],
+                   ["beans", 1],
+                   ["bread", 5],
+                   ["kale", 5]];
+
+addItem = function(item, quantity){
+  for (i = 0; i < groceryList.length; i++){
+    if (groceryList[i] == item){
+      groceryList[i]
+    };
+    else{
+      groceryList.push([item, quantity])
+    };
+};
+
+removeItem = function(item){
+  for (i = 0; i < groceryList.length; i++){
+    if (groceryList[i][0] == item)
+      groceryList.splice(i, 1);
+    else
+      console.log("Please enter an existing item to remove.")
+  };
+};
+
+console.log(groceryList);
